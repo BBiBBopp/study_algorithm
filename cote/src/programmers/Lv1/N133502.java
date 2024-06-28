3fa557c152f335bb120d763fa0d05a9d35a0a3e0
BBipBopp <dlwldms0895@gmail.com>
2024-06-28 20:18:10 +0900

    string VS stringBuilder 시간복잡도 차이

--- cote/src/programmers/Lv1/N133502.java
+++ cote/src/programmers/Lv1/N133502.java
@@ -64,12 +64,16 @@ public class N133502 {
     }
 	
 	/*
-	 * < String 연산 >
+	 * https://programmer-may.tistory.com/154
+	 * 
+	 * < String >
+	 * 
+	 * ● +, += 연산
 	 * + , += 를 사용하는 경우, + 할 때마다 새로운 객체를 만들어서 연산을 수행함 (기존의 데이터 변경이 아닌 새로운 리터럴 추가)
-	 * 시간복잡도 : O(N + K) ※ N: 기존 문자열의 길이, K: 더하려는 문자열의 길이
+	 * 시간복잡도 : O(기존 문자열의 길이 + 더하려는 문자열의 길이)
 	 * 
 	 * 예시)
-	   public static void sumString(String[] args) {
+	   public void sumString() {
 	        String str = "";
 	        int n = 1000000;
 	        for(int i = 0; i < n; i++){
@@ -77,13 +81,40 @@ public class N133502 {
  	        }
 	    }
 	    
-	    // 소요 시간 : (s의 길이 + "A" 의 길이) * N
-	      > N * (N+1) / 2 * N 
-	      > N * (N+1) / 2 는 1부터 N까지의 합
-	      > O(N^3)
-	        
+	    > 소요 시간 : (s의 길이 + "A" 의 길이) * N
+	    > N*(N+1)/2 * N  ( N*(N+1)/2 는 1부터 N까지의 합 )
+	    > O(N^3)
+	    
+	 * ● replace();
+	 * 시간복잡도 : O(문자열의 길이 * (교체할 문자열의 길이 + 교체되는 문자열의 길이/교체할 문자열의 길이))
 	 * 
-	 *  
+	 * 예시)
+	   str.replace(a, b)
+	   
+	   > 소요 시간 : O(len(str) * (len(a) + len(b)/len(a)))
+	      문자열 str의 길이만큼 전체에서 문자열을 하나씩 탐색하고, 교체할 문자열의 길이만큼 이전값을 탐색해서 교체해야하는 문자열인지 아닌지를 판별함
+	      교체해야 하는 경우, 교체되는 문자열의 길이에 따라 추가 연산
+	   1) 교체되는 문자열의 길이 == 교체할 문자열의 길이 
+	            교체되는 문자열의 길이만큼 연산 수행 : O(len(b)/len(a)) = O(1)
+	   2) 교체되는 문자열의 길이 > 교체할 문자열의 길이
+	            초과된 길이만큼 새 위치를 만들고 교체 수행 : O(len(b)/len(a)) 만큼의 추가 연산 소요
+
+	 * < StringBuilder >
+	 * ● append();
+	 * 시간복잡도 : O(N)
+	 * 
+	 * 예시)
+	   public void sumString() {
+	        StringBuider sb = new StringBuider();
+	        int n = 1000000;
+	        for(int i = 0; i < n; i++){
+	        	sb.append("A"); 
+ 	        }
+	    }
+	    
+	    > 소요 시간 : O(N)
+	 * 
+	 * 
 	 * 
 	 */
 
