76a525d7e5a43654caac33e1c7610edf641929a0
Jieun <dlwldms0895@gmail.com>
2025-09-16 10:14:40 +0900

    0916 code cata

---
+++ cote/src/sparta/product.java
@@ -0,0 +1,11 @@
+package sparta;
+/*
+    param   num1, num2
+    return  num1 * num2
+ */
+public class product {
+
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
